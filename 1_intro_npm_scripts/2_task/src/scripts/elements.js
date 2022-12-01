8142e8b805bc660bd1870d83cc6a188fbea68d84
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-01 17:12:55 +0200

    test add task 2

---
+++ 1_intro_npm_scripts/2_task/src/scripts/elements.js
@@ -0,0 +1,3 @@
+export const listElem = document.querySelector('.list');
+export const inputElem = document.querySelector('.task-input');
+export const btnElem = document.querySelector('.create-task-btn');
