21d5b9329a440ba97ba38d997de45556101538f3
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-06 23:02:27 +0200

    + path fix

--- 5_webpack_intro/2_task/webpack.config.js
+++ 5_webpack_intro/2_task/webpack.config.js
@@ -1,9 +1,12 @@
+const path = require('path');
+
 module.exports = {
   entry: {
     profile: './src/profile/index.js',
     dashboard: './src/dashboard/index.js',
-    },
-    output: {
-        filename: '[name].js'
-    }
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'build'),
+  },
 };
